7bbdab7ad551151998e2039ffc4d0177529da5a5
Eric Pinedo <epinedo@ucsd.edu>
2017-08-07 00:42:31 -0700

    need testing

--- phone.js
+++ phone.js
@@ -5,24 +5,6 @@
 
 
 
-/* 
-var prompts = require('./prompts.js');
-console.log(prompts.login(hello));
- 
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-rl.question( hello, (answer) => {
-  // TODO: Log the answer in a database
-  console.log(`Thank you for your valuable feedback: ${answer}`);
-
-  rl.close();
-});
-*/
 
 
 
