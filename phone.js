29d4b465b6bf30eda906c5271df5b26bd9fddcf0
Eric Pinedo <epinedo@ucsd.edu>
2017-08-08 23:24:57 -0700

    need to purchase numbers from twilio

--- phone.js
+++ phone.js
@@ -1,6 +1,6 @@
 /* Author: Eric Pinedo
  * Date: August 3, 2017
- * File Description: The main file which will hold all of the 'phone cannon' logic 
+ * File Description: The main file which will hold all of the 'phone flooding' logic 
  */
 
 
@@ -15,6 +15,7 @@
 
 
 var info = require('./config.js');
+var prompts = require('./prompts.js');
 console.log(info);
 
 var accountSid = info.MY_KEY;
@@ -22,17 +23,48 @@ var authToken = info.SECREY_KEY;
 
 
 
-
 var client = require('twilio')(accountSid, authToken);
+
+ 
+const readline = require('readline');
+
+const read = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+var hello = prompts.enterNumber + prompts.numberStart;
+read.question( hello, (answer) => {
+
+  console.log("Number entered: " answer);
+
+ read.close();
+});
+
+
+
+
 
 
 while( true ) { 
-client.calls.create({
-    url: "http://demo.twilio.com/docs/voice.xml",
-    to: info.to,
-    from: info.from
-}, function(err, call) {
-    process.stdout.write(call.sid);
-});
+
+	//loop fromNumbers
+
+	Call( fromNumber )
+
+	//error handling
+
 
 }
+
+function Call( fromNumber ){
+	client.calls.create({
+    	url: "http://demo.twilio.com/docs/voice.xml",
+    	to: answer
+    	from: fromNumber
+	}, function(err, call) {
+    	process.stdout.write(call.sid);
+	});
+
+}
+
